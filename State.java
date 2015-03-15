e698e74652b6c0791efed389d7cf953c9745f7fa
Andrew Schwartzmeyer <andrew@schwartzmeyer.com>
2015-03-14 18:58:08 -0700

    Merge branch 'game-state'

---
+++ State.java
@@ -0,0 +1,7 @@
+// Processing CAN use enums, if they're in a Java file!
+public enum State {
+  PAUSED,  // user paused
+  RUNNING, // in a wave
+  STAGED,  // between waves
+  MENU,    // user in menu
+}
